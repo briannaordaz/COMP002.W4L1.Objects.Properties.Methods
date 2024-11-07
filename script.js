ce8cea17d3c72a34f0c84e2c21de62a280a257fc
briannaordaz <briannaordaz5@gmail.com>
2024-11-06 20:13:46 -0800

    added author, purpose, and did step 1

---
+++ script.js
@@ -0,0 +1,18 @@
+// 1. Instantiate a new array and add 5 numeric values to it.
+// - Inside of a console.log(...) statement, output the first value of the array.
+// - Inside of a console.log(...) statement, output the last value of the array.
+// - What numbers did you have to pass as indexes? Why?
+let myArray = [2, 3, 8, 24, 25];
+
+console.log(myArray[0]);
+
+// 2. Using a for loop containing a console.log(...) statement, output each of the values in the array.
+
+
+// 3. Use the push() method to add 5 more values to your array. 
+// - What are the contents of your array now? Where did the new values appear?
+// - Use the pop() statement 3 times on your array. What are the contents of your array now?
+
+
+// 4. Declare a new string variable “myString” and place a string of your choosing inside.
+// - Output the length of the string inside of a console.log(...) statement.
